84568f328271330ac894c1fea62d4c69f5798bf4
kicker123 <chriskne@gmail.com>
2017-02-13 15:58:37 +0000

    Initial commit

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("sap.training.controller.Main", {
+		
+		onInit: function() {
+			this.getView().bindElement("/Products(1)");
+		}
+
+	});
+});
